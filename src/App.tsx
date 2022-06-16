4786398254aa2a9c39e55fbb3d7330bd2b548c77
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-06-16 13:17:47 -0300

    App styles and tasks wrapper made

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,86 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
+import { Header } from './components/Header';
+import { Task } from './components/Task';
+
+import './App.scss';
 import './styles/global.scss';
 
+interface Task {
+  id: number,
+  type: string;
+  description: string;
+  isDone: boolean;
+}
+
+const mockedTasks = [
+  {
+    id: 0,
+    type: 'family',
+    description: 'Buscar Julia Maria no Shopping',
+    isDone: false,
+  },
+  {
+    id: 1,
+    type: 'pets',
+    description: 'Buscar Woody no PetShop',
+    isDone: false,
+  },
+  {
+    id: 2,
+    type: 'house',
+    description: 'Recolher lixos',
+    isDone: false,
+  },
+]
+
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>(mockedTasks);
+
+  function switchTaskStatus(id: number) {
+    let tempTasks = [...tasks];
+
+    tempTasks.forEach((task, index) => {
+      let tempTask = { ...task };
+
+      if (tempTask.id === id) {
+        tempTask.isDone = !tempTask.isDone;
+        tempTasks[index] = tempTask;
+      }
+    });
+
+    setTasks(tempTasks);
+  }
+
+  function editTask(id: number) {
+
+  }
+
+  function deleteTask(id: number) {
+    console.log('deleteTask');
+    let tempTasks = [...tasks];
+
+    tempTasks.filter(task => task.id === id);
+
+    setTasks(tempTasks);
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
+      <Header />
+      <div className='app-content'>
+        {
+          tasks && tasks.length > 0 ? (
+            <div className='tasks-wrapper'>
+              {
+                tasks.map((task, key) => (
+                  <Task key={key} task={task} switchTaskStatus={switchTaskStatus} editTask={editTask} deleteTask={deleteTask} />
+                ))
+              }
+            </div>
+          ) : <p>Nenhuma tarefa adicionada ate o momento.</p>
+        }
+      </div>
     </div>
   )
 }
